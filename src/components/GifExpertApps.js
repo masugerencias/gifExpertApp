666de382e97d3bafef73698b3f24c598d85c6c9f
masugerencias <yannpoirotsanz@gmail.com>
2022-09-09 15:48:57 +0200

    creating components

---
+++ src/components/GifExpertApps.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+
+function GifExpertApps() {
+
+
+
+    const categories = ['Pokemon', 'Digimon', 'DragonBall'];
+
+    return (
+        <div>
+            <h2>GifExpertApps</h2>
+            <hr />
+            <button>Agregar</button>
+
+            {/* solo se pueden metodos , asi que se necesita un map para imprimir la lista */}
+
+            <ol> {categories.map(category => {
+                return <li key={category}>{category}  </li>
+            })
+            }</ol>
+
+
+                {/* El map es una pasada aunque no lo controle muy bien.
+                Del map podemos sacar dos parámetros por defecto, el elemento y su indice. */}
+        </div>
+    )
+}
+
+export default GifExpertApps
+
+
+/* Dentro del return en solo se puede poner expresiones de javascript que devuelvan un valor o METODOS! pero no ifs ni esas cosas. */
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GifExpertApps from './components/GifExpertApps';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <GifExpertApps  />
+);
+
